012f4f151abcf8966308fea281c1d2b558e73a22
edwinsdjah <edwinsdjah@gmail.com>
2022-11-02 15:24:17 +0700

    asdas

--- Jersey-Store/js/common.js
+++ Jersey-Store/js/common.js
@@ -32,28 +32,6 @@ $('#carouselExample').on('slide.bs.carousel', function (e) {
   }
 });
 
-$(document).ready(function () {
-  $('.customer-logos').slick({
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 1500,
-    arrows: false,
-    dots: false,
-    pauseOnHover: false,
-    responsive: [{
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 4
-      }
-    }, {
-      breakpoint: 520,
-      settings: {
-        slidesToShow: 3
-      }
-    }]
-  });
-});
 
 
 
