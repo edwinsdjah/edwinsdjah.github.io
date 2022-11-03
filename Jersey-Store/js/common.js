34b486d35077341dbb79baa03f5acbddf0cf19db
edwinsdjah <edwinsdjah@gmail.com>
2022-11-03 07:42:35 +0700

    asdasdsd

--- Jersey-Store/js/common.js
+++ Jersey-Store/js/common.js
@@ -1,6 +1,6 @@
-// $("#header").load("component/header.html");
-// $("#sidebar").load("component/sidebar.html");
-// $("footer").load("component/footer.html");
+$("#header").load("component/header.html");
+$("#sidebar").load("component/sidebar.html");
+$("footer").load("component/footer.html");
 
 
 window.onscroll = function () {
@@ -14,31 +14,6 @@ function scrollFunction() {
     document.getElementById("header").style.fontSize = "28px";
   }
 };
-
-
-
-$('#carouselExample').on('slide.bs.carousel', function (e) {
-
-  var $e = $(e.relatedTarget);
-  var idx = $e.index();
-  var itemsPerSlide = 3;
-  var totalItems = $('.carousel-item').length;
-
-  if (idx >= totalItems - (itemsPerSlide - 1)) {
-    var it = itemsPerSlide - (totalItems - idx);
-    for (var i = 0; i < it; i++) {
-      // append slides to end
-      if (e.direction == "left") {
-        $('.carousel-item').eq(i).appendTo('.carousel-inner');
-      } else {
-        $('.carousel-item').eq(0).appendTo('.carousel-inner');
-      }
-    }
-  }
-});
-
-
-
 
 
 $(".panel-title1").click(function () {
