b95c730d36d7ae944a6df63a1a5d85cde8ba67ca
edwinsdjah <edwinsdjah@gmail.com>
2022-11-03 18:08:00 +0700

    ASDASD

--- Jersey-Store/js/common.js
+++ Jersey-Store/js/common.js
@@ -1,6 +1,37 @@
 $("#sidebar").load("component/sidebar.html");
 $("footer").load("component/footer.html");
 
+window.onscroll = function () {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    document.getElementById("header").style.fontSize = "20px";
+  } else {
+    document.getElementById("header").style.fontSize = "28px";
+  }
+};
+
+$(".panel-title1").click(function () {
+  $(".angle-down1").toggleClass("down");
+})
+$(".panel-title2").click(function () {
+  $(".angle-down2").toggleClass("down");
+})
+
+$(".panel-title3").click(function () {
+  $(".angle-down3").toggleClass("down");
+})
+
+$(".panel-title4").click(function () {
+  $(".angle-down4").toggleClass("down");
+})
+
+$(".panel-title5").click(function () {
+  $(".angle-down5").toggleClass("down");
+});
+
 $(document).ready(function () {
   $("#header").load("component/header.html", function () {
     const openShopCart = document.querySelector('#cart a');
@@ -21,41 +52,5 @@ $(document).ready(function () {
       cart.classList.toggle('hide');
       document.querySelector('body').classList.toggle('stopScrolling')
     }
-
   })
-});
-
-
-
-
-window.onscroll = function () {
-  scrollFunction()
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
-    document.getElementById("header").style.fontSize = "20px";
-  } else {
-    document.getElementById("header").style.fontSize = "28px";
-  }
-};
-
-
-$(".panel-title1").click(function () {
-  $(".angle-down1").toggleClass("down");
-})
-$(".panel-title2").click(function () {
-  $(".angle-down2").toggleClass("down");
-})
-
-$(".panel-title3").click(function () {
-  $(".angle-down3").toggleClass("down");
-})
-
-$(".panel-title4").click(function () {
-  $(".angle-down4").toggleClass("down");
-})
-
-$(".panel-title5").click(function () {
-  $(".angle-down5").toggleClass("down");
 });
