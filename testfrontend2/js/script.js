4d4f99a06c08429bbafcea13ed75cacfb8fd2a93
edwinsdjah <edwinsdjah@gmail.com>
2023-01-01 15:31:34 +0700

    asdasd

--- testfrontend2/js/script.js
+++ testfrontend2/js/script.js
@@ -11,18 +11,25 @@ window.addEventListener('load', function () {
 
 let burger = document.querySelector('.hamburger');
 
-// burger.addEventListener('click', function () {
-//     let c = document.querySelector('.nav-collapse ');
-//     c.classList.toggle('show');
-//     c.classList.add('blink');
-// })
+burger.addEventListener('click', function () {
+    let c = document.querySelector('.navbar');
+    let collapse = document.querySelector('.navbar-collapse');
+    burger.classList.toggle('is-active');
+    c.classList.toggle('stretch');
+    document.querySelector('body').classList.toggle('no-scroll');
+    collapse.classList.add('blink');
+    collapse.classList.toggle('ie-show');
+    setTimeout(function () {
+        collapse.classList.remove('blink');
+    }, 500);
+});
 
 function spOnly() {
     const sec = document.getElementById('section1');
     const nav = document.querySelector('.navbar');
 
     window.onload = function () {
-        if (window.innerWidth < 787) {
+        if (window.innerWidth <= 768) {
             let h = nav.clientHeight;
             sec.style.paddingTop = h + 'px';
         } else {
@@ -31,14 +38,13 @@ function spOnly() {
     }
 
     window.addEventListener('resize', function () {
-        if (window.innerWidth < 787) {
+        if (window.innerWidth <= 768) {
             let h = nav.clientHeight;
             sec.style.paddingTop = h + 'px';
         } else {
             sec.style.paddingTop = '0';
         }
     })
-
 
     setTimeout(showPage, 2000);
     return
@@ -53,23 +59,23 @@ function showPage() {
     c.style.opacity = '1'
 }
 
-function navBack() {
-    const nav = document.querySelector('.navbar');
-    const btn = document.querySelector('.navbar-toggler');
-    const dark = document.querySelector('.nav-black');
-    const light = document.querySelector('.nav-light');
-    const burg = document.querySelector('.hamburger');
-    const body = document.querySelector('body');
+// function navBack() {
+//     const nav = document.querySelector('.navbar');
+//     const btn = document.querySelector('.navbar-toggler');
+//     const dark = document.querySelector('.nav-black');
+//     const light = document.querySelector('.nav-light');
+//     const burg = document.querySelector('.hamburger');
+//     const body = document.querySelector('body');
 
-    btn.addEventListener('click', function () {
-        nav.classList.toggle('show');
-        dark.classList.toggle('hide');
-        light.classList.toggle('hide');
-        burg.classList.toggle('is-active');
-        body.classList.toggle('no-scroll')
-        // c.classList.toggle('show');
-    });
-};
+//     btn.addEventListener('click', function () {
+//         nav.classList.toggle('show');
+//         dark.classList.toggle('hide');
+//         light.classList.toggle('hide');
+//         burg.classList.toggle('is-active');
+//         body.classList.toggle('no-scroll')
+//         // c.classList.toggle('show');
+//     });
+// };
 
 function scroll() {
     let nav = document.querySelector(".navbar");
@@ -90,10 +96,13 @@ let tabindex = 1;
 showTab(tabindex);
 
 function plusTab(n) {
+    let contId;
     if (tabindex !== 5 && n > 0) {
+        contId = 2018
         showTab(tabindex += n);
         console.log(tabindex)
     } else if (tabindex <= 5 && tabindex !== 1 && n < 0) {
+        contId = 2022
         showTab(tabindex += n);
         console.log(tabindex)
     }
@@ -103,7 +112,6 @@ function plusTab(n) {
 function selectTab() {
     let sel = document.querySelector('select');
     let opt = document.querySelectorAll('.tab-option');
-
 
     sel.addEventListener('change', function () {
         tabindex = this.selectedIndex + 1;
@@ -136,22 +144,12 @@ function selectTab() {
 
 }
 
-function tabcontent (tabindex){
-    
-}
-
-
 function showTab(n) {
     let tab = document.querySelectorAll('.tab-pane');
     let opt = document.querySelectorAll('.tab-option');
     let next = document.querySelector('.next');
     let prev = document.querySelector('.prev');
-    
-
-    console.log(tabindex);
-    
-    
-
+    let contId = opt[tabindex - 1].getAttribute('value');
     if (n >= tab.length) {
         next.style.opacity = 0;
     } else {
@@ -177,17 +175,80 @@ function showTab(n) {
         }
     }
 
-    
-
     tab[tabindex - 1].classList.add('active');
     tab[tabindex - 1].classList.add('blink');
     opt[tabindex - 1].setAttribute('selected', '');
     setTimeout(function () {
         tab[tabindex - 1].classList.remove('blink')
     }, 500);
-    
-    
-    
+    getTabContentbyId(contId);
+}
+
+function openTab(event, id) {
+    let i, tabcontent, tablink;
+    tabcontent = document.querySelectorAll('.tab-pane');
+    tablink = document.querySelectorAll('.nav-link');
+    let contId = document.getElementById(id);
+    for (let i = 0; i < tabcontent.length; i++) {
+        if (tabcontent[i].classList.contains('active')) {
+            tabcontent[i].classList.remove('active');
+        }
+    }
+
+    for (let i = 0; i < tablink.length; i++) {
+        if (tablink[i].classList.contains('active')) {
+            tablink[i].classList.remove('active');
+        }
+    }
+
+    contId.classList.add('active');
+    contId.classList.add('blink');
+    setTimeout(function () {
+        contId.classList.remove('blink')
+    }, 500);
+    event.target.classList.add('active');
+
+    getTabContentbyId(id);
+    // event.currentTarget.className += 'active';
+}
+
+function getTabContentbyId(id) {
+    let lowercontent = document.querySelector('.lower-tab-content .content h2');
+    let lowercaption = document.querySelector('.lower-tab-content .caption');
+    let lowerdate = document.querySelector('.lower-tab-content .content .date');
+    let tabBg = document.querySelector('.tab-bg')
+    switch (id) {
+        case '2018':
+            lowercontent.textContent = 'Lorem ipsum dolor sit amet, consectetur cing elit.';
+            lowercaption.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam porta sodales leo ibulum. Nullam vel posuere nisi. Donec in lobortis arcu.'
+            lowerdate.textContent = '28';
+            tabBg.style.backgroundImage = 'url(/img/tab-bg-1.png)';
+            break;
+        case '2019':
+            lowercontent.textContent = 'Lorem ipsum dolor sit amet, consectadipiscing elit.';
+            lowercaption.textContent = 'Lorem iolor sit amet, consectetur adipiscing elit. Nam porta sodales leo vitae vestibulum. Nullam vel posuere nisi. Donec in lobortis arcu.'
+            lowerdate.textContent = '27';
+            tabBg.style.backgroundImage = 'url(/img/tab-bg-2.png)';
+            break;
+        case '2020':
+            lowercontent.textContent = 'Lorem ipsum dolor sit amet, ur adipiscing elit.';
+            lowercaption.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam porta soditae vestibulum. Nullam vel posuere nisi. Donec in lobortis arcu.'
+            lowerdate.textContent = '38';
+            tabBg.style.backgroundImage = 'url(/img/tab-bg-3.png)';
+            break;
+        case '2021':
+            lowercontent.textContent = 'Lorem i dolor sit amet, consectetur adipiscing elit.';
+            lowercaption.textContent = 'Lorem ipsum dolt, consectetur adipiscing elit. Nam porta sodales leo vitae vestibulum. Nullam vel posuere nisi. Donec in lobortis arcu.'
+            lowerdate.textContent = '58';
+            tabBg.style.backgroundImage = 'url(/img/tab-bg-4.png)';
+            break;
+        case '2022':
+            lowercontent.textContent = 'Lorem ipsum dolor sit amet, consectetur elit.';
+            lowercaption.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam porta soe vestibulum. Nullam vel posuere nisi. Donec in lobortis arcu.'
+            lowerdate.textContent = '19';
+            tabBg.style.backgroundImage = 'url(/img/tab-bg-5.png)';
+            break;
+    }
 }
 
 
@@ -245,86 +306,24 @@ function showSlides(n) {
 }
 
 
-
-function openTab(event, id) {
-    let i, tabcontent, tablink;
-    tabcontent = document.querySelectorAll('.tab-pane');
-    tablink = document.querySelectorAll('.nav-link');
-    let lowercontent = document.querySelector('.lower-tab-content .content h2');
-    let lowercaption = document.querySelector('.lower-tab-content .caption');
-    let lowerdate = document.querySelector('.lower-tab-content .content .date')
-    let contId = document.getElementById(id);
-    for (let i = 0; i < tabcontent.length; i++) {
-        if (tabcontent[i].classList.contains('active')) {
-            tabcontent[i].classList.remove('active');
-        }
-    }
-
-    for (let i = 0; i < tablink.length; i++) {
-        if (tablink[i].classList.contains('active')) {
-            tablink[i].classList.remove('active');
-        }
-    }
-
-
-
-    contId.classList.add('active');
-    contId.classList.add('blink');
-    setTimeout(function () {
-        contId.classList.remove('blink')
-    }, 500);
-    event.target.classList.add('active');
-    
-    switch (id) {
-        case '2018':
-            lowercontent.textContent = 'Lorem ipsum dolor sit amet, consectetur cing elit.';
-            lowercaption.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam porta sodales leo ibulum. Nullam vel posuere nisi. Donec in lobortis arcu.'
-            lowerdate.textContent = '28';
-            break;
-        case '2019':
-            lowercontent.textContent = 'Lorem ipsum dolor sit amet, consectadipiscing elit.';
-            lowercaption.textContent = 'Lorem iolor sit amet, consectetur adipiscing elit. Nam porta sodales leo vitae vestibulum. Nullam vel posuere nisi. Donec in lobortis arcu.'
-            lowerdate.textContent = '27';
-            break;
-        case '2020':
-            lowercontent.textContent = 'Lorem ipsum dolor sit amet, ur adipiscing elit.';
-            lowercaption.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam porta soditae vestibulum. Nullam vel posuere nisi. Donec in lobortis arcu.'
-            lowerdate.textContent = '38';
-            break;
-        case '2021':
-            lowercontent.textContent = 'Lorem i dolor sit amet, consectetur adipiscing elit.';
-            lowercaption.textContent = 'Lorem ipsum dolt, consectetur adipiscing elit. Nam porta sodales leo vitae vestibulum. Nullam vel posuere nisi. Donec in lobortis arcu.'
-            lowerdate.textContent = '58';
-            break;
-        case '2022':
-            lowercontent.textContent = 'Lorem ipsum dolor sit amet, consectetur elit.';
-            lowercaption.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam porta soe vestibulum. Nullam vel posuere nisi. Donec in lobortis arcu.'
-            lowerdate.textContent = '19';
-            break;
-    }
-    
-    // event.currentTarget.className += 'active';
-}
-
-
 // Read more
 let btnmore = document.querySelector('.btn-more');
 
-btnmore.addEventListener('click',function(){
-    let moretext= document.querySelector('.more-text')
-    if(btnmore.textContent === 'Read More'){
+btnmore.addEventListener('click', function () {
+    let moretext = document.querySelector('.more-text')
+    if (btnmore.textContent === 'Read More') {
         moretext.style.display = 'inline';
         btnmore.textContent = 'Read Less';
-     } else if (btnmore.textContent === 'Read Less'){
+    } else if (btnmore.textContent === 'Read Less') {
         moretext.style.display = 'none';
         btnmore.textContent = 'Read More';
-     }
-    
+    }
+
 })
 
 scroll();
 // dropDown();
 spOnly();
-navBack();
+// navBack();
 // sel();
 selectTab();
