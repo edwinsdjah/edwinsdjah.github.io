9215d4a93a785a5925178d54c9ab0053a2d1c754
edwinsdjah <edwinsdjah@gmail.com>
2023-02-09 22:37:58 +0700

    test

---
+++ pukultikus/js/script.js
@@ -0,0 +1,54 @@
+const holes = document.querySelectorAll('.hole');
+const moles = document.querySelectorAll('.mole');
+const scoreValue = document.querySelector('.scoreValue');
+let lastHole;
+let isTimeUp = false;
+let score = 0;
+
+// random time to pop up
+function randomTime(min,max){
+    return Math.round(Math.random() * (max-min) + min);
+}
+
+function randomHole(holes){
+    const index = Math.floor(Math.random() * holes.length);
+    const hole = holes[index];
+    // if hole index same as last index, it will return to start new function
+    if(hole === lastHole){
+        return randomHole(holes)
+    }
+    lastHole = hole;
+    return hole;
+}
+
+
+function peep(){
+    // get randome time to determine how long mole peep
+    const time = randomTime(1000,1500); 
+    // get random hole from randomhole function
+    const hole = randomHole(holes);
+    hole.classList.add('up');
+    setTimeout(function(){
+        hole.classList.remove('up');
+        if(!isTimeUp){
+            peep();
+        }
+    },time);
+}
+
+function whack(e){
+    score++;
+    // this refer to item clicked
+    this.parentNode.classList.remove('up');
+    scoreValue.textContent = score;
+}
+
+
+peep();
+setTimeout(function(){
+    isTimeUp = true
+},15000)
+
+moles.forEach(function(element){
+    element.addEventListener('click',whack)
+})
