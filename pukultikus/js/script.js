4e3c48b73822e5f2b6969886a7d07798ce92c3a9
edwinsdjah <edwinsdjah@gmail.com>
2023-02-10 00:34:35 +0700

    asdsda

--- pukultikus/js/script.js
+++ pukultikus/js/script.js
@@ -4,6 +4,45 @@ const scoreValue = document.querySelector('.scoreValue');
 let lastHole;
 let isTimeUp = false;
 let score = 0;
+let min;
+let max;
+
+
+function selectLevel(){
+    const menu = document.querySelector('.menu');
+    const main = document.querySelector('.mainContent');
+    let id;
+    let container = document.querySelector('.menu .content .row');
+    container.addEventListener('click',function(e){
+        click.play();
+        mainMusic.play();
+        if (e.target.classList.contains('level')){
+            id = e.target.id
+            console.log(id)
+        } else {
+            return
+        }
+        switch(id){
+            case 'easy':
+            min = 800;
+            max = 1500;
+            break;
+            case 'medium':
+            min = 500;
+            max = 1000;
+            break;
+            case 'hard':
+            min = 300;
+            max = 800;
+        }
+        menu.classList.remove('show');
+        menu.classList.add('none');
+        main.classList.remove('none');
+        main.classList.add('show');
+        peep();
+    })
+    return
+}
 
 // random time to pop up
 function randomTime(min,max){
@@ -24,10 +63,11 @@ function randomHole(holes){
 
 function peep(){
     // get randome time to determine how long mole peep
-    const time = randomTime(500,800); 
+    const time = randomTime(min,max); 
     // get random hole from randomhole function
     const hole = randomHole(holes);
     hole.classList.add('up');
+    appear.play();
     setTimeout(function(){
         hole.classList.remove('up');
         if(!isTimeUp){
@@ -38,6 +78,7 @@ function peep(){
 
 function whack(e){
     score++;
+    hit.play();
     let mole = this
     mole.classList.add('vibrate');
     setTimeout(function(){
@@ -48,12 +89,35 @@ function whack(e){
     scoreValue.textContent = score;
 }
 
-
-peep();
-setTimeout(function(){
-    isTimeUp = true
-},15000)
-
 moles.forEach(function(element){
     element.addEventListener('click',whack)
 })
+
+
+selectLevel();
+setTimeout(function(){
+    isTimeUp = true
+},24000)
+
+
+const appear = new Howl({
+    src: ['sound/peep.mp3'],
+    html5: true
+})
+
+const hit = new Howl({
+    src: ['sound/whack.mp3'],
+    html5: true
+})
+
+const click = new Howl({
+    src: ['sound/click.wav'],
+    html5: true
+})
+
+const mainMusic = new Howl({
+    src: ['sound/soundtrack.mp3'],
+    html5: true
+})
+
+
