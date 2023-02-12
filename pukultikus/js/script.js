f53ce95d16d31232fa8afaf50330c656c2686aa4
edwinsdjah <edwinsdjah@gmail.com>
2023-02-12 12:39:32 +0700

    aette

--- pukultikus/js/script.js
+++ pukultikus/js/script.js
@@ -1,47 +1,59 @@
 const holes = document.querySelectorAll('.hole');
 const moles = document.querySelectorAll('.mole');
 const scoreValue = document.querySelector('.scoreValue');
+const restartWindow = document.querySelector('.restartWindow');
+const main = document.querySelector('.mainContent');
+const menu = document.querySelector('.menu');
 let lastHole;
 let isTimeUp = false;
 let score = 0;
 let min;
 let max;
-
+const restartBtn = document.querySelector('.btn-warning');
 
 function selectLevel(){
-    const menu = document.querySelector('.menu');
-    const main = document.querySelector('.mainContent');
     let id;
-    let container = document.querySelector('.menu .content .row');
-    container.addEventListener('click',function(e){
-        click.play();
-        mainMusic.play();
-        if (e.target.classList.contains('level')){
-            id = e.target.id
-            console.log(id)
-        } else {
-            return
-        }
-        switch(id){
-            case 'easy':
-            min = 800;
-            max = 1500;
-            break;
-            case 'medium':
-            min = 500;
-            max = 1000;
-            break;
-            case 'hard':
-            min = 300;
-            max = 800;
-        }
-        menu.classList.remove('show');
-        menu.classList.add('none');
-        main.classList.remove('none');
-        main.classList.add('show');
-        peep();
-    })
-    return
+    let container = document.querySelectorAll('.menu .content .row a img');
+    
+    container.forEach(function(e){
+        e.addEventListener('click',function(e){
+            click.play();
+            mainMusic.play();
+            if (e.target.classList.contains('level')){
+                id = e.target.parentNode.id
+                console.log(id)
+            } else {
+                return
+            }
+            switch(id){
+                case 'easy':
+                min = 800;
+                max = 1500;
+                break;
+                case 'medium':
+                min = 500;
+                max = 1000;
+                break;
+                case 'hard':
+                min = 300;
+                max = 800;
+            }
+            menu.classList.remove('show');
+            menu.classList.add('none');
+            main.classList.remove('none');
+            main.classList.add('show');
+            peep();
+            setTimeout(function(){
+                isTimeUp = true
+                mainMusic.stop();
+                main.classList.remove('show');
+                main.classList.add('none');
+                restartWindow.classList.add('show');
+                restartWindow.classList.remove('none')
+            },24000)
+        })
+        return
+       })
 }
 
 // random time to pop up
@@ -93,11 +105,17 @@ moles.forEach(function(element){
     element.addEventListener('click',whack)
 })
 
+restartBtn.addEventListener('click',function(){
+    click.play();
+    isTimeUp = false;
+    score = 0
+    scoreValue.textContent = score
+    menu.classList.remove('none');
+    menu.classList.add('show');
+    restartWindow.classList.add('none'); 
+    restartWindow.classList.remove('show');
+})
 
-selectLevel();
-setTimeout(function(){
-    isTimeUp = true
-},24000)
 
 
 const appear = new Howl({
@@ -121,3 +139,4 @@ const mainMusic = new Howl({
 })
 
 
+selectLevel();
